a58e84a342c23d559c2600e6657e8670381a5bbb
Cheng Li <cli04@email.wm.edu>
2014-12-15 09:34:32 -0500

    Test

--- src/menzies/attack/Attack.java
+++ src/menzies/attack/Attack.java
@@ -4,7 +4,7 @@ public class Attack {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		System.out.println("Not implemented yet.");
 	}
 
 	public static String[] queryGenerator(String tableName, int n){
